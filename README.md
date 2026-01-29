341c4fd14c0d73834e75b189b7dec596a9f1e7fa
tanvia <tanvia@Tanvis-Surface.localdomain>
2026-01-29 14:47:18 -0600

    dependencies + initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Setup Instructions
+
+### 1. Create a virtual environment
+```bash
+python3 -m venv venv
+source venv/bin/activate
+
+### 2. Install requirements
+```bash
+pip3 install -r requirements.txt
---
+++ main.py
@@ -0,0 +1,9 @@
+import logging
+import time
+
+import cflib.crtp
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.crazyflie.log import LogConfig
+from cflib.positioning.motion_commander import MotionCommander
+from cflib.utils import uri_helper
